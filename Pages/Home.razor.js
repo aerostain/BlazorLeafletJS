ceaedd28e49655719b28589c9fdd89800af967cb
Rick MarSal <richims026@gmail.com>
2024-07-22 11:20:18 -0500

    Init

---
+++ Pages/Home.razor.js
@@ -0,0 +1,10 @@
+﻿export function initialD() {
+    var pmap = L.map('imap').setView([-12.08, -77.0], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19
+    }).addTo(pmap);
+
+
+    console.log("Hi Godzilla");
+}
